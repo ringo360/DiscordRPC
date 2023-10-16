a71ffdc5b5ca3cf26e6c7a5d1ddcf0cf0dc6bb7f
ringo360 <105296365+ringo360@users.noreply.github.com>
2023-10-16 22:18:40 +0900

    process.exit()

--- main.js
+++ main.js
@@ -10,13 +10,29 @@ rl.question('[1] Create RPC\n' + '[2] Edit RPC\n' + '[3] Delete RPC\n' + '[4] Se
 	const number = parseInt(ans);
 
 	if (isNaN(number)) {
-		console.log('Invalid input! Please enter a number(1~5)!')
-	} else if (number >= 1 && number <= 9) {
+		console.log('Invalid input. Please enter a number(1~5)!')
+	} else if (number >= 1 && number <= 5) {
 		switch (number) {
 			case 1:
 				console.log("1");
 				break;
-			//TODO: 書け
+			case 2:
+				console.log("2");
+				break;
+			case 3:
+				console.log("3");
+				break;
+			case 4:
+				console.log("4");
+				break;
+			case 5:
+				console.log("(5)Exit");
+				process.exit()
+
+			default:
+				console.log(`detect ${number}`);
 		}
+	} else {
+		console.log('Invalid input. Please enter a number(1~5)!')
 	}
 })
